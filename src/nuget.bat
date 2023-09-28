fe28a2ad3402d4e5c9e7e6cd21854bd7518d5521
Parham <parhaamsaremi@gmail.com>
2023-09-28 15:07:07 +0300

    fix wildcard

--- src/nuget.bat
+++ src/nuget.bat
@@ -198,10 +198,13 @@ dir
 echo "ccccccccccccccccccccccccccccc"
 set DIR=%~dp0
 set OUTPUTDIR=%~dp0\packages
-for /R %DIR% %%a in (*.nupkg) do xcopy "%%a" "%OUTPUTDIR%\*"
+if not exist "%OUTPUTDIR%" (
+    mkdir "%OUTPUTDIR%"
+)
+for /R %DIR% %%a in (*.nupkg) do xcopy "%%a" "%OUTPUTDIR%"
 xcopy *.nupkg packages /s /y
 
-for /R %DIR% %%a in (*.snupkg) do xcopy "%%a" "%OUTPUTDIR%\*"
+for /R %DIR% %%a in (*.snupkg) do xcopy "%%a" "%OUTPUTDIR%"
 xcopy *.snupkg packages /s /y
 
 EXIT /B %ERRORLEVEL%
